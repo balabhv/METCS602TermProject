7699aa7af13b6b27127034a245aa3057e627ecea
Virinchi <virinchi.balabhadrapatruni@gmail.com>
2018-05-03 23:41:13 -0400

    no message

--- index.js
+++ index.js
@@ -164,10 +164,9 @@ app.post('/addSection', function(req, res) {
 	var prof_id = req.body.prof_id;
 	var clssrm_id = req.body.clssrm_id;
 	var size_lmt = req.body.size_lmt;
-	console.log(typeof req.body.start_tm);
-	var start_tm = req.body.start_tm.toISOString();
+	var start_tm = req.body.start_tm;
 	start_tm = start_tm.substring(start_tm.indexOf("T") + 1);
-	var end_tm = req.body.end_tm.toISOString();
+	var end_tm = req.body.end_tm;
 	end_tm = end_tm.substring(end_tm.indexOf("T") + 1);
 	var m = req.body.m;
 	var t = req.body.t;
